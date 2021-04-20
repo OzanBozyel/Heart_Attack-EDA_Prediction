a50e3061af2109e1a5fad434e90c86a28aee10b1
OzanBozyel <ozanbozyel_lis@hotmail.com>
2021-04-20 21:03:06 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 20 19:38:52 2021
+
+@author: Ozan
+"""
+
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+import seaborn as sns
+import plotly_express as px
+from scipy.stats.stats import pearsonr
+
+data = pd.read_csv("C:/Users/Ozan/Desktop/heart_attack/heart.csv")
+
+
+
+df = data.rename(columns={'trtbps':'rest_bps','thalachh':'thal',
+                              'exng':'exang','caa':'ca','output':'mark'})
+
+
+
+df1 = df.drop(labels=['slp','thall'], axis=1, inplace=False)
+
+
+print(df1.isnull().sum())
+
+
+corr_Pearson = df1.corr(method="pearson")
+
+figure = plt.figure(figsize=(12,8))
+sns.heatmap(corr_Pearson,vmin=-1,vmax=+1,cmap='Blues',annot=True, 
+            linewidths=1,linecolor = 'white')
+plt.title('Pearson Correlation')
+plt.show()
+
+
+
+
+
+
+
