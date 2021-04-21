da292831a7d770b007b1f5e6ccf62a2110240b60
OzanBozyel <ozanbozyel_lis@hotmail.com>
2021-04-21 19:55:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,11 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 import seaborn as sns
-import plotly_express as px
 from scipy.stats.stats import pearsonr
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression,LinearRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
 
 data = pd.read_csv("C:/Users/Ozan/Desktop/heart_attack/heart.csv")
 
@@ -29,6 +32,8 @@ columns = df1.columns
 print(df1.isnull().sum())
 
 
+
+
 corr_Pearson = df1.corr(method="pearson")
 
 figure = plt.figure(figsize=(12,8))
@@ -38,7 +43,7 @@ plt.title('Pearson Correlation')
 plt.show()
 
 
-
+'''
 df1.hist(figsize=(15,10))
 plt.show()
 
@@ -51,7 +56,73 @@ for i in columns:
         plt.show()
     
 
+figure = plt.figure(figsize=(15,10))
+sns.barplot(x='sex', y='mark',data=df1)
+plt.show()
 
+figure = plt.figure(figsize=(15,10))
+sns.barplot(x='cp', y='mark',data=df1)
+plt.show()
+
+figure = plt.figure(figsize=(15,10))
+sns.barplot(x='fbs', y='mark',data=df1)
+plt.show()
+
+figure = plt.figure(figsize=(15,10))
+sns.barplot(x='restecg', y='mark',data=df1)
+plt.show()
+
+figure = plt.figure(figsize=(15,10))
+sns.barplot(x='exang', y='mark',data=df1)
+plt.show()
+
+figure = plt.figure(figsize=(15,10))
+sns.barplot(x='ca', y='mark',data=df1)
+plt.show()
+
+
+
+
+figure = plt.figure(figsize=(15,10))
+sns.boxplot(x='chol',y='mark',orient='h',data=df1)
+plt.show()
+
+figure = plt.figure(figsize=(15,10))
+sns.boxplot(x='rest_bps',y='mark',orient='h',data=df1)
+plt.show()
+
+figure = plt.figure(figsize=(15,10))
+sns.boxplot(x='thal',y='mark',orient='h',data=df1)
+plt.show()
+
+figure = plt.figure(figsize=(15,10))
+sns.boxplot(x='oldpeak',y='mark',orient='h',data=df1)
+plt.show()
+
+'''
+
+data_x = df.drop(labels=['mark'], axis=1, inplace=False)
+data_y = df['mark']
+
+
+x_train, x_test, y_train, y_test = train_test_split(data_x, data_y ,
+                                    test_size = 0.3, random_state = 44, 
+                                    shuffle = True)
+
+
+
+
+
+Linear_R = LinearRegression().fit(x_train,y_train)
+
+Logistic_R = LogisticRegression(solver="liblinear").fit(x_train,y_train)
+
+KNN = KNeighborsClassifier().fit(x_train,y_train)
+
+Decision_Tree = DecisionTreeClassifier(random_state=44).fit(x_train,y_train)
+
+
+models = ['Linear_R','Logistic_R','KNN','Decision_Tree']
 
 
 
