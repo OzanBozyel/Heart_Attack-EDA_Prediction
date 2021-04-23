69d5de0963504ff53d4c4351375aade2d2509004
OzanBozyel <ozanbozyel_lis@hotmail.com>
2021-04-23 20:38:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,36 +6,31 @@ Created on Tue Apr 20 19:38:52 2021
 """
 
 import pandas as pd
-import numpy as np
 from matplotlib import pyplot as plt
 import seaborn as sns
-from scipy.stats.stats import pearsonr
 from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.linear_model import LogisticRegression,LinearRegression
+from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
+from sklearn.metrics import confusion_matrix
 
 data = pd.read_csv("C:/Users/Ozan/Desktop/heart_attack/heart.csv")
 
 
-
+# Data Manipulation
 df = data.rename(columns={'trtbps':'rest_bps','thalachh':'thal',
                               'exng':'exang','caa':'ca','output':'mark'})
 
 
-
 df1 = df.drop(labels=['slp','thall'], axis=1, inplace=False)
-
 columns = df1.columns
-
 
 print(df1.isnull().sum())
 
 
 
-
-corr_Pearson = df1.corr(method="pearson")
+# Data Visualization
+corr_Pearson = df1.corr(method='pearson')
 
 figure = plt.figure(figsize=(12,8))
 sns.heatmap(corr_Pearson,vmin=-1,vmax=+1,cmap='Blues',annot=True, 
@@ -44,7 +39,7 @@ plt.title('Pearson Correlation')
 plt.show()
 
 
-'''
+
 df1.hist(figsize=(15,10))
 plt.show()
 
@@ -100,8 +95,8 @@ figure = plt.figure(figsize=(15,10))
 sns.boxplot(x='oldpeak',y='mark',orient='h',data=df1)
 plt.show()
 
-'''
 
+# Prediction
 data_x = df.drop(labels=['mark'], axis=1, inplace=False)
 data_y = df['mark']
 
@@ -113,40 +108,44 @@ x_train, x_test, y_train, y_test = train_test_split(data_x, data_y ,
 
 
 
-Linear_R = LinearRegression().fit(x_train,y_train)
-
 Logistic_R = LogisticRegression(solver="liblinear").fit(x_train,y_train)
 
 KNN = KNeighborsClassifier().fit(x_train,y_train)
 
-Decision_Tree = DecisionTreeClassifier(random_state=44).fit(x_train,y_train)
+Decision_Tree = DecisionTreeClassifier(random_state=44).fit(x_train,
+                                                            y_train)
+
+models = [Logistic_R,KNN,Decision_Tree]
 
 
-models = [Linear_R,Logistic_R,KNN,Decision_Tree]
 
-
-
-total = pd.DataFrame(columns=['Score','Model'])
+model_total = pd.DataFrame(columns=['Score','Model'])
 for model in models:
-    predict = model.predict(x_test)
+    pre = model.predict(x_test)
     cvs = cross_val_score(model,x_test,y_test).mean()
-    name = model.__class__.__name__
-    result = pd.DataFrame([cvs,name],columns=['Score','Model'])
-    print(model)
-    print(cvs)
-    print('*'*20)
-    total.append(result)
-
-'''
-score = pd.DataFrame(dict(Score = total, Model = models))
-best_score = score.sort_values(by='Score',ascending = False)
-best_model = best_score.iloc[:1]['Model']
-
-best_predict = best_model.predict(x_test)
-evaluating_score = cross_val_score(best_model,x_test,y_test).mean()
+    
+    cf_matrix = confusion_matrix(y_test,pre)
+    figure = plt.figure(figsize=(15,10))
+    sns.heatmap(cf_matrix, linecolor = "white", linewidth = 1, 
+                cmap = "Blues", annot = True)
+    plt.show()
+    
+    model_name = model.__class__.__name__ # tricky
+    result = pd.DataFrame([[cvs,model_name]],columns=['Score','Model'])
+    model_total = model_total.append(result)
 
 
-'''
+figure = plt.figure(figsize=(15,10))   
+sns.barplot(x="Model",y="Score",data=model_total)
+plt.ylim(0,1)
+
+best_model = model_total.sort_values(by=['Score'],ascending=False).iloc[0,1]
+
+
+
+
+
+
 
 
 
